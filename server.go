4e88804591b32c7dddf62e743f77b99ff0f1017b
aditya <dev@chimeracoder.net>
2013-12-27 13:43:26 -0500

    Remove unnecessary functions

--- server.go
+++ server.go
@@ -5,11 +5,8 @@ import (
 	"flag"
 	"fmt"
 	"github.com/gorilla/mux"
-	"github.com/gorilla/schema"
-	"github.com/gorilla/sessions"
 	"log"
 	"net/http"
-	"os"
 	"text/template"
 )
 
@@ -17,32 +14,7 @@ const PROFILE_SESSION = "profile"
 
 var (
 	httpAddr        = flag.String("addr", ":8000", "HTTP server address")
-	baseTmpl string = "templates/base.tmpl"
-	store           = sessions.NewCookieStore([]byte(COOKIE_SECRET)) //CookieStore uses secure cookies
-	decoder         = schema.NewDecoder()                            //From github.com/gorilla/schema
-
-	//The following three variables can be defined using environment variables
-	//to avoid committing them by mistake
-
-	COOKIE_SECRET = []byte(os.Getenv("COOKIE_SECRET"))
 )
-
-func renderTemplate(w http.ResponseWriter, name string, data interface{}, filenames ...string) {
-	s1, _ := template.ParseFiles(filenames...)
-	s1.ExecuteTemplate(w, name, data)
-}
-
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	serveAngularHome(w, r)
-}
-
-func serveBase(w http.ResponseWriter, r *http.Request) {
-	s1, err := template.ParseFiles("templates/base.tmpl", "templates/index.tmpl")
-	if err != nil {
-		panic(err)
-	}
-	s1.ExecuteTemplate(w, "base", nil)
-}
 
 // renderAngularTemplate sets the delimiters for the specificed template(s) to be "[[" and "]]"
 // and then parses and renders all templates specified by the filenames
